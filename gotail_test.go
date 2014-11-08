4d5ee47f8750251f8cf3084a27838b443c46c05b
David Leung <davleun@gmail.com>
2014-11-07 21:59:38 -0800

    Added benchmark test

--- gotail_test.go
+++ gotail_test.go
@@ -1,9 +1,11 @@
 package gotail
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
+	"runtime"
 	"sync"
 	"testing"
 	"time"
@@ -15,10 +17,10 @@ var fname string = "test.log"
 var wg sync.WaitGroup
 
 func TestAppendFile(t *testing.T) {
-	CreateFile("")
-	defer RemoveFile()
+	createFile("")
+	defer removeFile()
 
-	tail, err := NewTail(fname, Config{timeout: 10})
+	tail, err := NewTail(fname, Config{Timeout: 10})
 	assert.Equal(t, err, nil)
 
 	var line string
@@ -31,7 +33,7 @@ func TestAppendFile(t *testing.T) {
 		return
 	}()
 
-	WriteFile("foobar\n")
+	writeFile("foobar\n")
 
 	<-done
 
@@ -45,7 +47,7 @@ func TestWriteNewFile(t *testing.T) {
 	done := make(chan bool)
 
 	go func() {
-		tail, _ = NewTail(fname, Config{timeout: 10})
+		tail, _ = NewTail(fname, Config{Timeout: 10})
 
 		line = <-tail.Lines
 		done <- true
@@ -53,10 +55,10 @@ func TestWriteNewFile(t *testing.T) {
 	}()
 
 	time.Sleep(10 * time.Millisecond) // Allow the listener to fully setup
-	CreateFile("")
-	defer RemoveFile()
+	createFile("")
+	defer removeFile()
 
-	WriteFile("foobar\n")
+	writeFile("foobar\n")
 
 	<-done
 
@@ -70,50 +72,102 @@ func TestRenameFile(t *testing.T) {
 
 	// Sets up background tailer
 	go func() {
-		tail, _ = NewTail(fname, Config{timeout: 10})
+		tail, _ = NewTail(fname, Config{Timeout: 10})
 
 		line = <-tail.Lines
 		done <- true
 		return
 	}()
 
-	CreateFile("")
-	RenameFile()
+	createFile("")
+	renameFile()
 
 	_, err := os.Stat(fname)
 	assert.Equal(t, true, os.IsNotExist(err))
 
 	time.Sleep(10 * time.Millisecond) // Allow the listener to fully setup
-	CreateFile("foobar\n")
+	createFile("foobar\n")
 
 	<-done
 
 	assert.Equal(t, "foobar", line)
 
 	_ = os.Remove(fname + "_new")
-	RemoveFile()
+	removeFile()
 }
 
-func TestNewTail(t *testing.T) {
-	_, err := NewTail(fname, Config{timeout: 0})
+func TestNoFile(t *testing.T) {
+	_, err := NewTail(fname, Config{Timeout: 0})
 	assert.Equal(t, true, os.IsNotExist(err))
 }
 
-func CreateFile(contents string) {
+func TestBenchmark(t *testing.T) {
+	log.Println("Running Benchmarks")
+	var concurrency int = 2
+	var rowcount = 500000           // number of rows to write
+	runtime.GOMAXPROCS(concurrency) // number of writer processes
+	var row int
+
+	_ = os.Remove(fname)
+	createFile("")
+
+	f, err := os.OpenFile(fname, os.O_APPEND|os.O_WRONLY, 0600)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	tail, err := NewTail(fname, Config{Timeout: 10})
+	time.Sleep(10 * time.Millisecond)
+
+	for i := 0; i < concurrency; i++ {
+		go func(i int) {
+			log.Printf("Spawned %d Workers\n", i)
+
+			for row <= rowcount {
+				body := fmt.Sprintf("%d Worker doing the write.  %d iteration is being written.\n", i, row)
+				writeContents(f, body)
+				row++
+			}
+		}(i)
+	}
+
+	count := 0
+	startTime := time.Now()
+
+	for _ = range tail.Lines {
+		count++
+		if count == rowcount {
+			break
+		}
+	}
+
+	duration := time.Since(startTime).Seconds()
+	fmt.Printf("%d rows processed in %0.4f seconds, at the rate of %0.4f rows/s\n", count, duration, float64(count)/duration)
+	removeFile()
+}
+
+func writeContents(f *os.File, contents string) {
+	_, err := f.WriteString(contents)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func createFile(contents string) {
 	err := ioutil.WriteFile(fname, []byte(contents), 0600)
 	if err != nil {
 		log.Fatalln(err)
 	}
 }
 
-func RemoveFile() {
+func removeFile() {
 	err := os.Remove(fname)
 	if err != nil {
 		log.Fatalln(err)
 	}
 }
 
-func RenameFile() {
+func renameFile() {
 	oldname := fname
 	newname := fname + "_new"
 	err := os.Rename(oldname, newname)
@@ -122,14 +176,11 @@ func RenameFile() {
 	}
 }
 
-func WriteFile(contents string) {
+func writeFile(contents string) {
 	f, err := os.OpenFile(fname, os.O_APPEND|os.O_WRONLY, 0600)
 	if err != nil {
 		log.Fatalln(err)
 	}
 	defer f.Close()
-	_, err = f.WriteString(contents)
-	if err != nil {
-		log.Fatalln(err)
-	}
+	writeContents(f, contents)
 }
