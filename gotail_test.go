711be0d9637bff69c29175a42ccd74714e7f892c
David Leung <davleun@gmail.com>
2014-11-08 09:19:26 -0800

    Moved benchmark to separate folder

--- gotail_test.go
+++ gotail_test.go
@@ -1,11 +1,9 @@
 package gotail
 
 import (
-	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
-	"runtime"
 	"sync"
 	"testing"
 	"time"
@@ -99,51 +97,6 @@ func TestRenameFile(t *testing.T) {
 func TestNoFile(t *testing.T) {
 	_, err := NewTail(fname, Config{Timeout: 0})
 	assert.Equal(t, true, os.IsNotExist(err))
-}
-
-func TestBenchmark(t *testing.T) {
-	log.Println("Running Benchmarks")
-	var concurrency int = 2
-	var rowcount = 10000            // number of rows to write
-	runtime.GOMAXPROCS(concurrency) // number of writer processes
-	var row int
-
-	_ = os.Remove(fname)
-	createFile("")
-
-	f, err := os.OpenFile(fname, os.O_APPEND|os.O_WRONLY, 0600)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	tail, err := NewTail(fname, Config{Timeout: 10})
-	time.Sleep(10 * time.Millisecond)
-
-	for i := 0; i < concurrency; i++ {
-		go func(i int) {
-			log.Printf("Spawned %d Workers\n", i)
-
-			for row <= rowcount {
-				body := fmt.Sprintf("%d Worker doing the write.  %d iteration is being written.\n", i, row)
-				writeContents(f, body)
-				row++
-			}
-		}(i)
-	}
-
-	count := 0
-	startTime := time.Now()
-
-	for _ = range tail.Lines {
-		count++
-		if count == rowcount {
-			break
-		}
-	}
-
-	duration := time.Since(startTime).Seconds()
-	fmt.Printf("%d rows processed in %0.4f seconds, at the rate of %0.4f rows/s\n", count, duration, float64(count)/duration)
-	removeFile()
 }
 
 func writeContents(f *os.File, contents string) {
